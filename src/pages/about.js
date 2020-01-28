d62279f88e9d58f79582d89a5458cdb7ce6fd7b0
Amanda Chesin <33710232+achesin@users.noreply.github.com>
2020-01-28 11:21:00 -0500

    updated about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,8 +13,10 @@ const SecondPage = () => (
 			<div class="about-textbox">
         <h1>About Me</h1>
         <p>Hey! I'm Amanda, and I'm a photographer based in New York City. Originally from Arizona, I got my start in photography in 2018 at the University of Arizona's student-run radio station, KAMP Radio. I photographed one of my favorite bands, The Aces, and saw the magic of being creative in a concert environment even if I wasn't the one performing on stage. </p>
-        <p>More than a year later, I've photographed over 20 shows between Arizona and NYC and have delved into the world of portrait photography with many of Broadway's rising stars</p>
-        <p>Let's shoot together! Fill out the contact form or message me on Instagram and let's set something up!</p>
+        <p>More than a year later, I've photographed over 20 shows between Arizona and NYC and have delved into the world of portrait photography with many of Broadway's rising stars.</p>
+        <p>My greatest goal is to capture your joy. Let's connect and create something special- send me an email and we'll get started!</p>
+	<p>Participating in my first showcase at Melrose Ballroom in NYC on Wednesday, January 29th from 7-11pm with RAW Artists! Looking forward to showing my stuff!</p>
+	<p>Magazine Feature</p>
       </div>
     </section>
     </div>
