dea37a47f3db4c4b21fb231a21fab7a54350aee5
Amanda Chesin <achesin@email.arizona.edu>
2020-01-28 16:36:16 -0500

    changed styling on portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,15 +3,16 @@ import SEO from "../components/seo"
 import TalkingHeader from '../components/talkingheader'
 import ArticleList from '../components/article-list'
 import Footer from '../components/footer'
+import Like from '../components/like'
 
 export default () => (
     <div>
         <SEO title="Portfolio" />
         <TalkingHeader />
         <div class="container">
-            <h1>Portfolio page</h1>
+            <h1>My Portfolio</h1>
             <ArticleList />
         </div>
-        <Footer />
+        <Like />
     </div>
 )
